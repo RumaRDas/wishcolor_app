cda815b117aa21b8305639d419d1b393b233cdea
Ruma Das <rumaadl16@gmail.com>
2020-07-17 00:36:17 +0930

    adding nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app =express();
 
 const PORT = process.env.PORT || 4000;
+
+// Define middleware here
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    try {
+        res.send(" Data deleted successfuly");
+   
+    } catch (error) {
+        console.log(" error.message");
+        return res.status(500);
+    }
+})
 
 // Start the API server
 app.listen (PORT, () =>{
