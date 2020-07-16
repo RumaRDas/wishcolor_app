9aaec52d457fc6292ef01659ac97ac49533004a4
Ruma Das <rumaadl16@gmail.com>
2020-07-16 23:04:00 +0930

    working on server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const express = require('express');
+
+const app = express();
+const PORT = 4000;
+app.get('/', (req, res) => res.json({hello: "world"}));
+app.listen(PORT, console.log(`server started on port ${PORT}`));
