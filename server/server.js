727efdc69aa171f8de5154dfaa9d92e408d60b30
Ruma Das <rumaadl16@gmail.com>
2020-07-16 23:11:56 +0930

    adding server.js

--- server/server.js
+++ server/server.js
@@ -2,5 +2,18 @@ const express = require('express');
 
 const app = express();
 const PORT = 4000;
+
 app.get('/', (req, res) => res.json({hello: "world"}));
+
+app.use((req, res, next) => {
+    const err = new Error('Not found');
+    err.status =404;
+    next(err);
+});
+// middleware
+app.use((err,req, res,next) =>{
+    res.status(err.ststus || 500).json({
+        err: err.message || 'Something went wrong'
+    });
+});
 app.listen(PORT, console.log(`server started on port ${PORT}`));
