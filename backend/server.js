15abc0dd4b5955858e9560d06d066904600e4486
Ruma Das <rumaadl16@gmail.com>
2020-07-17 01:34:10 +0930

    connected with mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,39 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
-const app =express();
 
+
+const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Define middleware here
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-    try {
-        res.send(" Data deleted successfuly");
-   
-    } catch (error) {
-        console.log(" error.message");
-        return res.status(500);
-    }
-})
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config()
+    // app.use(express.static("client/build"));
+}
+
+app.get('/', (req, res) => {
+    res.send(" server connected successfuly .....\n");
+});
+
+// Connect to the Mongo DB
+try {
+    mongoose.connect(process.env.MONGO_DB_CONNECTION, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    console.log('Mongodb connected successfuly !')
+} catch (error) {
+    console.log(" error.message");
+ 
+}
+
 
 // Start the API server
-app.listen (PORT, () =>{
+app.listen(PORT, () => {
     console.log(`API Server now listening on PORT ${PORT}!`)
 })
