c4a9ba9a6df1037c966f980311a3c901a23a5d85
Ruma Das <rumaadl16@gmail.com>
2020-07-17 13:49:51 +0930

    adding api for gradient

--- backend/config/upload.js
+++ backend/config/upload.js
@@ -1,8 +1,13 @@
 const multer = require('multer');
-const path  = require('path');
+const path = require('path');
 
-module.exports ={
+module.exports = {
     storage: multer.diskStorage({
-        destination: path.resolve(__dirname, "..","..", "files")
+        destination: path.resolve(__dirname, "..", "..", "files"),
+        filename: (req, file, callback) => {
+            const ext = path.extname(file.originalname)
+            const name = path.basename(file.originalname, ext)
+            callback(null, `${name.replace(/\s/g, "")}-${Date.now()}${ext}`)
+        }
     })
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const multer = require('multer');
+
 const routes = express.Router();
 const UserController = require('../controllers/UserController');
+const GradientController = require('../controllers/GradientController');
+
+//Uploading file
+const uploadConfig = require('../config/upload')
+const upload = multer(uploadConfig);
 
 routes.get('/', (req, res) => {
 	res.send('Hello from Node.js app \n')
 })
 
+//Gradient
+routes.post('./gradient', upload.single('thumbnail'),GradientController.createGradient )
+//user
 routes.post('/register', UserController.createUser)
 routes.get('/user/:userId', UserController.getUserById)
 
