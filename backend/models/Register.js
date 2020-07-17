c8bd559e17573c9a04dfbd48b5151a66abfed06b
Ruma Das <rumaadl16@gmail.com>
2020-07-18 01:08:37 +0930

    adding  a registration model

---
+++ backend/models/Register.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const RegisterSchema = new mongoose.Schema({
+    date: String,
+    approved: Boolean,
+    color: String,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+ gradient:{
+     type: mongoose.Schema.Types.ObjectId,
+     ref: "Gradient"
+
+ }
+})
+module.exports = mongoose.model('User', RegisterSchema);
