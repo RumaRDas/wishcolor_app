909ef0606dced998cb973de1b242fcaf4f60018b
Ruma Das <rumaadl16@gmail.com>
2020-07-17 11:08:59 +0930

    adding error message for same user

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -4,11 +4,26 @@ const User = require('../models/User')
 module.exports = {
 	async store(req, res) {
 		try {
-			const { email, firstName, lastName, password } = req.body
-			
-	console.log(req.body)
-		} catch (err) {
-			throw Error(`Error while Registering new user :  ${err}`)
+			const { email, firstName, lastName, password } = req.body;
+
+			const existentUser = await User.findOne({ email });
+			if (!existentUser) {
+				const user = await User.create({
+					firstName: firstName,
+					lastName: lastName,
+					password: password,
+					email: email
+				});
+				return res.json(user);
+			}
+
+			return res.status(400).json({
+				message: "email/user already exist! do you want to login?"
+			})
+
+
+		} catch (error) {
+			throw Error(`Error while Registering new user :  ${error}`)
 		}
 	},
 }
