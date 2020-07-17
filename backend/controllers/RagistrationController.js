37003792533c699c16b9fec3ba4c0f062348bf7a
Ruma Das <rumaadl16@gmail.com>
2020-07-18 01:49:39 +0930

    adding registration controller and api

--- backend/controllers/RagistrationController.js
+++ backend/controllers/RagistrationController.js
@@ -1,18 +1,22 @@
 const Registration = require('../models/Registration')
-const { createUser } = require('./UserController')
 
 module.exports ={
 
     async create(req, res) {
         const { user_id } = req.headers;
-        const { gradientid } = req.headers;
+        const { gradientId } = req.params;
         const { date } = req.body;
 
         const registration = await Registration.create({
             user: user_id,
-            gradient: gradientid,
+            gradient: gradientId,
             date
         })
+        await registration
+        .populate(`user`)
+        .populate(`gradient`)
+        .execPopulate();
+
         return res.json(registration);
     }
 
