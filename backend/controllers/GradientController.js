473f96f3def15faa9cb8d966713fb5c7fd607915
Ruma Das <rumaadl16@gmail.com>
2020-07-17 13:25:57 +0930

    adding multer and create config file for fileuload

--- backend/controllers/GradientController.js
+++ backend/controllers/GradientController.js
@@ -1,14 +1,30 @@
-// const Gradient = require('../models/Gradient');
+const Gradient = require('../models/Gradient');
+const User = require('../models/User');
 
-// module.exports ={
-//    async createGradient(req, res){
-//        try{
-//            const { title, description, price} = req.body;
-//            const{}
+module.exports = {
+    async createGradient(req, res) {
+        try {
+            const { title, description, price } = req.body;
+            const { userId } = req.headers;
+            const {filename} = req.file;
 
-//        }catch(error){
-//         throw Error(`Error while Creating  new Gradient :  ${error}`)
-//        }
-//    }
+            const user = await User.findById(userId)
 
-// }
+            if (!user) {
+                return res.status(422).json({ message: 'User Id does not exists' })
+            }
+
+            const gradient = await Gradient.create({
+                title,
+                description,
+                price,
+                user: userId,
+                thumbnail: filename
+            })
+            return res.json(gradient);
+        } catch (error) {
+            throw Error(`Error while Creating  new Gradient :  ${error}`)
+        }
+    }
+
+}
